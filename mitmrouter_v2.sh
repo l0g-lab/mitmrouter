b28aad4ef44629d6c72aa526b0ced49eed790d2b
l0g-lab <l0g-lab@users.noreply.github.com>
2025-06-24 22:13:21 -0400

    optmizations

--- mitmrouter_v2.sh
+++ mitmrouter_v2.sh
@@ -2,25 +2,57 @@
 # author: l0g
 # This allows you to use either a wireless access point or a wired connection for MITM IoT devices
 
-# Script Global Constants
-## Network Information
+# Network Information
 WIFI_SSID="mitm.io"
 WIFI_PASSWORD="1234567890"
-BR_IFACE="br0" # Used for WIRED mode only
 LAN_IP="192.168.150.1"
 LAN_DNS_SERVER="1.1.1.1"
 
-## Logging Setup
+# Logging Setup
 DATE_NOW=$(date +%F)
-LOG_FILE="mitm-log-$DATE_NOW.txt"
+LOG_FILE="log-$DATE_NOW.mitmrouter.txt"
 echo "" > $LOG_FILE
 
-## Config Files
+# Config Files
 DHCPD_CONF="tmp_dhcpd.conf"
 HOSTAPD_CONF="tmp_hostapd.conf"
 
-# Function Declarations
-## Network Calculator
+# Usage function
+usage() {
+    echo "AP MODE: $0 ap <wireless_interface> <out_interface>"
+    echo "WIRED MODE: $0 wired <wired_interface> <out_interface>"
+    exit 0
+}
+
+# Check script runs as root
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        log "Error: This script must be run as root"
+        exit 0
+    fi
+}
+
+# Check for required tools
+check_requirements() {
+    local tools=("ip" "iptables" "systemctl" "hostapd" "dhcpd")
+    for tool in "${tools[@]}"; do
+        if ! command -v "$tool" &>/dev/null; then
+            log "Error: Required tool '$tool' not found"
+            exit 0
+        fi
+    done
+}
+
+# Check network interfaces exist
+validate_interface() {
+    local iface=$1
+    if ! ip link show "$iface" &>/dev/null; then
+        log "Error: Interface $iface does not exist"
+        exit 0
+    fi
+}
+
+# Network Calculator
 generate_network_config() {
     local ip="$1"
     IFS='.' read -r i1 i2 i3 i4 <<< "$ip"
@@ -32,106 +64,69 @@ generate_network_config() {
     LAN_SUBNET="255.255.255.0"
 }
 
-## Usage function
-usage() {
-    echo 'AP MODE: ./mitmrouter.sh ap ${WIRELESS_INTERFACE} ${GATEWAY_INTERFACE}'
-    echo 'WIRED MODE: ./mitmrouter.sh wired ${WIRED_INTERFACE} ${GATEWAY_INTERFACE}'
+# Logging function
+log() {
+    local timestamp
+    timestamp=$(date +'%F %T')
+    echo "$timestamp: $1"
+    echo "$timestamp: $1" >> "$LOG_FILE"
+}
+
+# Cleanup function
+cleanup() {
+    local mode=$1
+    local in_interface=$2
+
+    log "[*] Cleanup started"
+
+    log "[*] Cleaning up $in_interface and IP assignments"
+    sudo ip link set dev $in_interface down
+    sudo ip addr flush dev $in_interface
+
+    log "[*] Stopping dhcpd"
+    sudo systemctl stop isc-dhcp-server
+    sudo mv /etc/dhcp/dhcpd.conf.bak /etc/dhcp/dhcpd.conf
+
+    if [[ "$mode" == 'wireless' ]]; then
+        log "[*] Stopping hostapd"
+        sudo pkill hostapd
+    fi
+
+    log "[*] Removing temporary files"
+    rm -f $DHCPD_CONF $HOSTAPD_CONF &>/dev/null
+
+    log "[*] Cleanup complete"
     exit 0
 }
 
-## Print time function
-log_time() {
-    date +'%F %T'
-}
-
-## Logging function
-log() {
-    echo "$(log_time): $1"
-    echo "$(log_time): $1" >> "$LOG_FILE"
-}
-
-## Cleanup function
-cleanup() {
-    if [[ "$1" == 'wireless' ]]; then
-        WIFI_INTERFACE=$2
-        log "[*] Cleanup started" 
-        log "[*] Cleaning up interfaces and IP assignments"
-        sudo ip link set dev $WIFI_INTERFACE down
-        sudo ip addr flush dev $WIFI_INTERFACE
-
-        log "[*] Stopping dhcpd"
-        sudo systemctl stop isc-dhcp-server
-
-        log "[*] Stopping hostapd"
-        sudo pkill hostapd
-
-        log "[*] Removing temporary files"
-        rm -rf $DHCPD_CONF $HOSTAPD_CONF
-        sudo mv /etc/dhcp/dhcpd.conf.bak /etc/dhcp/dhcpd.conf
-
-        log "[*] Cleanup complete"
-        exit 0
-    
-    elif [[ "$1" == 'wired' ]]; then
-        IN_INTERFACE=$2
-        log "[*] Cleanup started" 
-        log "[*] Cleaning up interfaces and IP assignments"
-        sudo ip link set dev $IN_INTERFACE down
-        sudo ip addr flush dev $IN_INTERFACE
-
-        # Bridge Stuff
-        #sudo ip link set $BR_IFACE down
-        #sudo ip addr flush dev $BR_IFACE
-        #sudo brctl delbr $BR_IFACE
-
-        log "[*] Stopping dhcpd"
-        sudo systemctl stop isc-dhcp-server
-        
-        log "[*] Removing temporary files"
-        rm -rf $DHCPD_CONF
-        sudo mv /etc/dhcp/dhcpd.conf.bak /etc/dhcp/dhcpd.conf
-    
-        log "[*] Cleanup complete"
-        exit 0
-    fi
-}
-
-## Clean and setup IP address on interface
+# Clean and setup IP address on interface
 clean_setup_ip() {
-    IN_INTERFACE=$2
-    if [[ "$1" == 'wireless' ]]; then
+    local mode=$1
+    local in_interface=$2
+    log "[*] Setting up IP addresses"
+    if [[ "$mode" == 'wireless' ]]; then
         # Clean
-        sudo ip link set dev $IN_INTERFACE down
-        sudo ip addr flush dev $IN_INTERFACE
+        sudo ip link set dev $in_interface down
+        sudo ip addr flush dev $in_interface
         # Setup
-        sudo ip link set dev $IN_INTERFACE up
-        sudo ip addr add $LAN_IP/24 dev $IN_INTERFACE
+        sudo ip link set dev $in_interface up
+        sudo ip addr add $LAN_IP/24 dev $in_interface
         sleep 1
-    elif [[ "$1" == 'wired' ]]; then
+    elif [[ "$mode" == 'wired' ]]; then
         # Clean
-        sudo ip link set $IN_INTERFACE down
-        sudo ip addr flush dev $IN_INTERFACE
-
-        # Bridge Stuff
-        #sudo ip link set $BR_IFACE down
-        #sudo ip addr flush dev $BR_IFACE
-        #sudo brctl delbr $BR_IFACE
+        sudo ip link set $in_interface down
+        sudo ip addr flush dev $in_interface
 
         # Setup
-        sudo ip link set dev $IN_INTERFACE up
-        sudo ip addr add $LAN_IP/24 dev $IN_INTERFACE
-
-        # Bridge Stuff
-        #sudo brctl addbr $BR_IFACE
-        #sudo brctl addbif $BR_IFACE $IN_INTERFACE
-        #sudo ip link set $BR_IFACE up
-        #sudo ip addr add $LAN_IP/24 dev $BR_IFACE
+        sudo ip link set dev $in_interface up
+        sudo ip addr add $LAN_IP/24 dev $in_interface
     fi
 }
 
-## Build dhcpd config and run
+# Build dhcpd config and run
 build_run_dhcpd() {
-    IN_INTERFACE=$1
+    local in_interface=$1
+    log "[*] Building dhcpd config"
     generate_network_config $LAN_IP
     echo "option domain-name \"$WIFI_SSID\";" > $DHCPD_CONF
     echo "option domain-name-servers $LAN_DNS_SERVER;" >> $DHCPD_CONF
@@ -145,7 +140,7 @@ build_run_dhcpd() {
     echo "}" >> $DHCPD_CONF
     sudo cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
     sudo cp $DHCPD_CONF /etc/dhcp/dhcpd.conf
-    sudo sed -i "/INTERFACESv4/ s/.*/INTERFACESv4=$IN_INTERFACE/" /etc/default/isc-dhcp-server
+    sudo sed -i "/INTERFACESv4/ s/.*/INTERFACESv4=$in_interface/" /etc/default/isc-dhcp-server
 
     # Stop and start dhcpd
     log "[*] Running dhcp server now...."
@@ -155,23 +150,26 @@ build_run_dhcpd() {
         log "[*] dhcpd started successfully."
     else
         log "[*] dhcpd startup failed. Exiting."
+        exit 0
     fi
 }
 
-## Flush iptables rules and configure for routing
+# Flush iptables rules and configure for routing
 clean_setup_firewall() {
-    IN_INTERFACE=$1
-    OUT_INTERFACE=$2
+    local in_interface=$1
+    local out_interface=$2
+    log "[*] Adding firewall rules"
     sudo iptables -F
     sudo iptables -t nat -F
-    sudo iptables -t nat -A POSTROUTING -o $2 -j MASQUERADE
+    sudo iptables -t nat -A POSTROUTING -o $out_interface -j MASQUERADE
     sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-    sudo iptables -A FORWARD -i $IN_INTERFACE -o $OUT_INTERFACE -j ACCEPT
+    sudo iptables -A FORWARD -i $in_interface -o $out_interface -j ACCEPT
 }
 
-## Configuring OS to be a router
+# Configuring OS to be a router
 setup_forwarding() {
-    sudo sysctl -w net.ipv4.ip_forward=1
+    log "[*] Setting up forwarding"
+    sudo sysctl -w net.ipv4.ip_forward=1 &>/dev/null
 }
 
 # Check for no args
@@ -179,72 +177,71 @@ if [ -z "$1" ]; then
     usage
 fi
 
-if [[ "$1" == 'ap' ]] && [[ "$2" == wl* ]] && ([[ "$3" == e* ]] || [[ "$3" == wl* ]]); then
-    # Wireless Setup
-    WIFI_INTERFACE=$2
-    GATEWAY_INTERFACE=$3
-    #AUTO_WIFI_INTERFACE=$(ip link show | grep -E 'wlan[0-9]+|wlx[a-f0-9]+' | awk '{print $2}' | tr -d ':')
+# Main function
+main() {
+    check_root
+    check_requirements
 
-    log "[*] mitmrouter_v2 - AP mode starting"
+    # Check number of arguments
+    [[ $# -ne 3 ]] && usage
 
-    log "[*] Setting up IP addresses"
-    clean_setup_ip wireless $WIFI_INTERFACE
- 
-    log "[*] Building dhcpd config"
-    build_run_dhcpd $WIFI_INTERFACE
- 
-    log "[*] Adding firewall rules"
-    clean_setup_firewall $WIFI_INTERFACE $GATEWAY_INTERFACE
+    local mode=$1
+    local in_interface=$2
+    local out_interface=$3
 
-    log "[*] Setting up forwarding"
-    setup_forwarding
- 
-    log "[*] Running hostapd now....."
-    echo "interface=$WIFI_INTERFACE" > $HOSTAPD_CONF
-    echo "driver=nl80211" >> $HOSTAPD_CONF
-    echo "country_code=US" >> $HOSTAPD_CONF
-    echo "ssid=$WIFI_SSID" >> $HOSTAPD_CONF
-    echo "hw_mode=g" >> $HOSTAPD_CONF
-    echo "channel=6" >> $HOSTAPD_CONF
-    echo "wpa=2" >> $HOSTAPD_CONF
-    echo "wpa_passphrase=$WIFI_PASSWORD" >> $HOSTAPD_CONF
-    echo "wpa_key_mgmt=WPA-PSK" >> $HOSTAPD_CONF
-    echo "wpa_pairwise=TKIP" >> $HOSTAPD_CONF
-    echo "rsn_pairwise=CCMP" >> $HOSTAPD_CONF
-    echo "auth_algs=1" >> $HOSTAPD_CONF
-    echo "macaddr_acl=0" >> $HOSTAPD_CONF
-    sudo hostapd -t $HOSTAPD_CONF
+    # Make sure interfaces exist
+    validate_interface "$in_interface"
+    validate_interface "$out_interface"
 
-    # Cleanup
-    cleanup wireless $WIFI_INTERFACE
+    # Setup Signal Handler
+    trap 'cleanup "$mode" "$in_interface"' SIGINT SIGTERM
 
-elif [[ "$1" == 'wired' ]] && [[ "$2" == e* ]] && ([[ "$3" == e* ]] || [[ "$3" == wl* ]]); then
-    # Wired Setup
-    IN_INTERFACE=$2
-    GATEWAY_INTERFACE=$3
+    if [[ "$mode" == 'ap' && "$in_interface" == wl* && ("$out_interface" == e* || "$out_interface" == wl*) ]]; then
+        # Wireless Setup
+        log "[*] mitmrouter_v2 - AP mode starting"
 
-    trap cleanup SIGINT
+        clean_setup_ip wireless $in_interface
+        build_run_dhcpd $in_interface
+        clean_setup_firewall $in_interface $out_interface
+        setup_forwarding
 
-    log "[*] mitmrouter_v2 - WIRED mode starting"
+        log "[*] Running hostapd now....."
+        echo "interface=$in_interface" > $HOSTAPD_CONF
+        echo "driver=nl80211" >> $HOSTAPD_CONF
+        echo "country_code=US" >> $HOSTAPD_CONF
+        echo "ssid=$WIFI_SSID" >> $HOSTAPD_CONF
+        echo "hw_mode=g" >> $HOSTAPD_CONF
+        echo "channel=6" >> $HOSTAPD_CONF
+        echo "wpa=2" >> $HOSTAPD_CONF
+        echo "wpa_passphrase=$WIFI_PASSWORD" >> $HOSTAPD_CONF
+        echo "wpa_key_mgmt=WPA-PSK" >> $HOSTAPD_CONF
+        echo "wpa_pairwise=TKIP" >> $HOSTAPD_CONF
+        echo "rsn_pairwise=CCMP" >> $HOSTAPD_CONF
+        echo "auth_algs=1" >> $HOSTAPD_CONF
+        echo "macaddr_acl=0" >> $HOSTAPD_CONF
+        sudo hostapd -t $HOSTAPD_CONF
 
-    log "[*] Setting up IP addresses"
-    clean_setup_ip wired $IN_INTERFACE
+        # Cleanup
+        cleanup wireless $in_interface
 
-    log "[*] Building dhcpd config"
-    build_run_dhcpd $IN_INTERFACE
+    elif [[ "$mode" == 'wired' && "$in_interface" == e* && ("$out_interface" == e* || "$out_interface" == wl*) ]]; then
+        # Wired Setup
+        log "[*] mitmrouter_v2 - WIRED mode starting"
+        clean_setup_ip wired $in_interface
+        build_run_dhcpd $in_interface
+        clean_setup_firewall $in_interface $out_interface
+        setup_forwarding
 
-    log "[*] Adding firewall rules"
-    clean_setup_firewall $IN_INTERFACE $GATEWAY_INTERFACE
+        # Keep the program open
+        log "[*] mitmrouter_v2 - WIRED mode running..."
+        sleep infinity
 
-    log "[*] Setting up forwarding"
-    setup_forwarding
+        # Cleanup
+        cleanup wireless $in_interface
 
-    # Keep the program open
-    sleep infinity
+    else
+        usage
+    fi
+}
 
-    # Cleanup
-    cleanup wired $IN_INTERFACE
-
-else
-    usage
-fi
+main "$@"
